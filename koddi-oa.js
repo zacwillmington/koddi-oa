af93d3019ce5fe868a9d0424b8afc8ff2f026f17
Zac Willmington <zac.willmington@gmail.com>
2021-07-06 15:05:17 -0500

    Update comment section

--- koddi-oa.js
+++ koddi-oa.js
@@ -1,6 +1,5 @@
 /*
 	Given a matrix and two variables k and z, find the sum of a single path that is equal to z and the path with the greatest sum larger than k. If both of these conditions are true, return true, else false.
-
 	A path can go in any direction, but cannot overlap itself and must start in the top left and end in the bottom right corner.
 
 	input:
@@ -15,7 +14,7 @@
 
 	answer: true
 
-	In the example below, I've put the answer to path with the greatest sum in with astrix.
+	In the example below, I've put the answer to path with the greatest sum in with asterisk for a visual representation.
 
 	matrix = [
 		[*-1, *3, *6],
@@ -25,9 +24,13 @@
 
 	Explanation:
 	Because the largest sum in the matrix = 13 (-1 + 3 + 6 + 1 + 4) which is greater than k(12) and
-	there is one path that sums to -12 and is made up of (-1 + 3 + -31 + 13 + 4). For the matrix provided above the two conditions are met, therefore the answer is true.
+	there is one path that sums to -12 and consists of (-1 + 3 + -31 + 13 + 4). For the matrix provided above the two conditions are met, therefore the answer is true.
 
-	Feel free to solve this problem in your programming language of choice. The expected results for each of the matricies are listed next to each. For example, running the the function with the three inputs will result in true, true and false.
+	Feel free to solve this problem in your programming language of choice. The expected results for each of the matrices are listed next to each.
+	For example, your algorithm should give the following results:
+		maxPathInMatrix(matrix1, 12, -12) => true
+ 		maxPathInMatrix(matrix2, 352, 3) => true
+ 		maxPathInMatrix(matrix3, 200, 14) => false
 */
 
 
