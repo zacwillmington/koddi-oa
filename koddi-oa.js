71183473fa1561608dcc73af5fa9f53bb4f32dc4
Zac Willmington <zac.willmington@gmail.com>
2021-07-06 16:12:51 -0500

    Feedback changes

--- koddi-oa.js
+++ koddi-oa.js
@@ -1,6 +1,6 @@
 /*
-	Given a matrix and two variables k and z, find the sum of a single path that is equal to z and the path with the greatest sum larger than k. If both of these conditions are true, return true, else false.
-	A path can go in any direction, but cannot overlap itself and must start in the top left and end in the bottom right corner.
+	Given a matrix and two variables `greaterThan` and `equalTo`, travese the matrix finding the sum of a single path that is equal to `equalTo` and the path with the greatest sum larger than `greaterThan`. If both of these conditions are true, return true, else false.
+	A path can go in any direction, but cannot overlap itself and must start in the top left and end in the bottom right corner. The allowed moves are: up, down, left and right.
 
 	input:
 	matrix = [
@@ -9,12 +9,12 @@
 		[12,-3, 5],
 	]
 
-	k = 12
-	z = -12
+	greaterThan = 12
+	equalTo = -12
 
 	answer: true
 
-	In the example below, I've put the answer to path with the greatest sum in with asterisk for a visual representation.
+	In the example below, I've put the answer to path with the greatest sum with asterisk for a visual representation.
 
 	matrix = [
 		[*-1, *3, *6],
@@ -23,7 +23,7 @@
 	]
 
 	Explanation:
-	Because the largest sum in the matrix = 13 (-1 + 3 + 6 + 1 + 4) which is greater than k(12) and
+	Because the largest sum in the matrix = 13 (-1 + 3 + 6 + 1 + 4) which is greater than greaterThan(12) and
 	there is one path that sums to -12 and consists of (-1 + 3 + -31 + 13 + 4). For the matrix provided above the two conditions are met, therefore the answer is true.
 
 	Feel free to solve this problem in your programming language of choice. The expected results for each of the matrices are listed next to each.
@@ -34,13 +34,13 @@
 */
 
 
-const matrix1 = [// expected: true when k = 12 and z = -12
+const matrix1 = [// expected: true when greaterThan = 12 and equalTo = -12
 	[-1 , 3, 6],
 	[-22,-31, 1],
 	[11, 13, 4],
 ];
 
-const matrix2 = [// expected: true when k = 352 and z = 3
+const matrix2 = [// expected: true when greaterThan = 352 and equalTo = 3
 	[-1 , 3, 6],
 	[-22,-31, 18],
 	[11, 13, 64],
@@ -50,17 +50,15 @@ const matrix2 = [// expected: true when k = 352 and z = 3
 	[-81 , 93, 6],
 ];
 
-const matrix3 = [// expected: false when k = 200 and z = 14
+const matrix3 = [// expected: false when greaterThan = 200 and equalTo = 14
 	[-1 , 3, 7],
 	[-22,-31, 1],
 	[11, 13, 4],
 	[-11, 136, 4],
 ];
 
-function maxPathInMatrix(matrix, k, z) {
+function maxPathInMatrix(matrix, greaterThan, equalTo) {
 	// Write your code here
-
-	console.log(matrix, k, z);
 }
 
 const resultTest1 = maxPathInMatrix(matrix1, 12, -12);// true
